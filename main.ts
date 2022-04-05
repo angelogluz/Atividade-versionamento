b9b2f8f7a66e7a8afb7d39feaa229ab56b5da57b
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-04-05 10:58:11 -0300

    :sparkles:Adiciona Hunter

--- main.ts
+++ main.ts
@@ -2,11 +2,17 @@ import { Priest } from "./Priest";
 import { Warrior } from "./Warrior";
 import { Mage } from "./Mage";
 import { Personagem } from "./Personagem";
+//Importação do Personagem Hunter
+import { Hunter } from "./Hunter";
 
 let mage: Personagem = new Mage("Angelo mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
+//Adiciona Personagem Hunter
+let hunter: Personagem = new Hunter("Grégory hunter");
 
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
 console.log("priest :>> ", priest);
+//Adiciona Personagem Hunter
+console.log("hunter :>> ", hunter);
