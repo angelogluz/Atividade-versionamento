1fb2d266d3e665335a54f1c9fce78b184f638a15
Vynicius Correa <oliveiravynicius@gmail.com>
2022-04-06 17:11:26 -0300

    :white_check_mark:: Adiciona propriedades privadas e altera valores de atributos da classe Hunter

--- Hunter.ts
+++ Hunter.ts
@@ -2,10 +2,14 @@ import { Personagem } from "./Personagem";
 import { Util } from "./Util";
 
 export class Hunter extends Personagem {
+    private _intelecto: number;
+    private _velocidade: number;
+    constructor(nome: string) {
     super(nome);
-        this._armadura = Util.randomizar(100, 1_000);
-        this._vidaMaxima = Util.randomizar(200, 1_000);
-        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
-        this._intelecto = Util.randomizar(100, 1_000);
-        this._velocidade = Util.randomizar(10, 2_000);
+        this._armadura = Util.randomizar(150, 1_000);
+        this._vidaMaxima = Util.randomizar(300, 1_000);
+        this._vidaAtual = Util.randomizar(100, this._vidaMaxima);
+        this._intelecto = Util.randomizar(50, 1_000);
+        this._velocidade = Util.randomizar(100, 2_000);
+    }
 }
