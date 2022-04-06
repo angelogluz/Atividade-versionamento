d535e30e6671aa39a210b1819d74cac4da417876
diego-rosa <diego.jose.rosa.da.silva@gmail.com>
2022-04-06 16:43:30 -0300

    ✅: Adiciona atributos a categoria Hunter

--- Hunter.ts
+++ Hunter.ts
@@ -2,5 +2,10 @@ import { Personagem } from "./Personagem";
 import { Util } from "./Util";
 
 export class Hunter extends Personagem {
-    
+    super(nome);
+        this._armadura = Util.randomizar(100, 1_000);
+        this._vidaMaxima = Util.randomizar(200, 1_000);
+        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._intelecto = Util.randomizar(100, 1_000);
+        this._velocidade = Util.randomizar(10, 2_000);
 }
