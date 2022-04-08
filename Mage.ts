d11ceee98a08be330972f0434cc4bc2765448166
Patricia Wentz <patriciawentzm@gmail.com>
2022-04-07 21:11:35 -0300

    :zap: Change max life and actual life

--- Mage.ts
+++ Mage.ts
@@ -7,8 +7,8 @@ export class Mage extends Personagem {
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
-        this._vidaMaxima = Util.randomizar(200, 1_000);
-        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._vidaMaxima = Util.randomizar(600, 1_000);
+        this._vidaAtual = Util.randomizar(80, this._vidaMaxima);
         this._intelecto = Util.randomizar(100, 1_000);
         this._velocidade = Util.randomizar(10, 2_000);
     }
