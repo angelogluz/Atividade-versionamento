07e968bd5c364edef87c7f4ea086d7843e861c9b
Diego Cardoso <diegosmc.ads@gmail.com>
2022-04-07 22:14:38 -0300

    :hammer: Import character hunter on main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Priest } from "./Priest";
 import { Warrior } from "./Warrior";
 import { Mage } from "./Mage";
+import { Hunter } from "./Hunter";
 import { Personagem } from "./Personagem";
 
 let mage: Personagem = new Mage("Angelo mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
+let hunter: Personagem = new Hunter("Angelo hunter");
 
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
