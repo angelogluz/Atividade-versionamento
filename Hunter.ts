9cfb2d474f7c54792b2617181dc7dd8465a4e723
JordanaRTexeira <jordana.keppler@hotmail.com>
2022-04-07 21:54:34 -0300

    :art: Implements specifics of the Hunter class

--- Hunter.ts
+++ Hunter.ts
@@ -2,14 +2,14 @@ import { Personagem } from "./Personagem";
 import { Util } from "./Util";
 
 export class Hunter extends Personagem {
-    private _intelecto: number;
-    private _velocidade: number;
+    private _poderes: number;
+    private _destreza: number;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
         this._vidaMaxima = Util.randomizar(200, 1_000);
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
-        this._intelecto = Util.randomizar(100, 1_000);
-        this._velocidade = Util.randomizar(10, 2_000);
+        this._poderes = Util.randomizar(100, 1_000);
+        this._destreza = Util.randomizar(10, 2_000);
     }
 }
