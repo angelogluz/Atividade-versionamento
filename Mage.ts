d737b819ebfd976f4a4cccae94392019a6ad3f96
paulo sisti <paulo.sisti@vortigo.com.br>
2022-04-07 21:51:15 -0300

    :sparkles: add mage_attribute
    add mage_attribute

--- Mage.ts
+++ Mage.ts
@@ -4,12 +4,9 @@ import { Util } from "./Util";
 export class Mage extends Personagem {
     private _intelecto: number;
     private _velocidade: number;
-<<<<<<< HEAD
     private _agilidade: number;
-    
-=======
     private _forca: number;
->>>>>>> a117b7c46426a3955d9a7fbc694698058ce9f160
+    private _mana: number;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
@@ -17,11 +14,9 @@ export class Mage extends Personagem {
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
         this._intelecto = Util.randomizar(100, 1_000);
         this._velocidade = Util.randomizar(10, 2_000);
-<<<<<<< HEAD
         this._agilidade = Util.randomizar(10, 1_000);
-=======
         this._forca = Util.randomizar(90, 2_000);
->>>>>>> a117b7c46426a3955d9a7fbc694698058ce9f160
+        this._mana = Util.randomizar(900, 2_000);
     }
 }
 
