95a44a4dc35e45dab34c3486229bd9625e5dfeea
lucas-raamos <l.oliveira260@gmail.com>
2022-04-07 21:35:06 -0300

    :zap: Atributo dinheiro
    Adicionar atributo dinheiro

--- Priest.ts
+++ Priest.ts
@@ -4,6 +4,7 @@ import { Util } from "./Util";
 export class Priest extends Personagem {
     private _espirito: number;
     private _versatilidade: number;
+    private _dinheiro: number;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
@@ -11,5 +12,6 @@ export class Priest extends Personagem {
         this._vidaAtual = Util.randomizar(20, this._vidaMaxima);
         this._espirito = Util.randomizar(100, 1_000);
         this._versatilidade = Util.randomizar(100, 1_000);
+        this._dinheiro = Util.randomizar(100, 1_000);
     }
 }
