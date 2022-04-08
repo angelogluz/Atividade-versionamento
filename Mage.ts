471730fd9576f6831f4b2d2c0cbdd978edd68b2f
rodrigo.wiebbelling <rodrigo.wiebbelling@thema.inf.br>
2022-04-08 15:02:01 -0300

    :art: Refactor Constructor

--- Mage.ts
+++ Mage.ts
@@ -5,7 +5,7 @@ export class Mage extends Personagem {
     private _intelecto: number;
     private _velocidade: number;
     private _vitalidade: number;
-    constructor(nome: string, vitalidade: number) {
+    constructor(nome: string, vitalidade: number, mana: number) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
         this._vidaMaxima = Util.randomizar(200, 1_000);
