9ea6ae5f56ef1a3c7c93f0dd2ac7f72fcc2aa00d
Carlos Lamego <clamego1@yahoo.com.br>
2022-04-09 16:20:21 -0300

    :memo: Adiciona Hunter ao main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Priest } from "./Priest";
 import { Warrior } from "./Warrior";
 import { Mage } from "./Mage";
+import { Hunter } from "./Hunter";
 import { Personagem } from "./Personagem";
 
 let mage: Personagem = new Mage("Angelo mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
+let hunter: Personagem = new Hunter("Clamego Hunter")
 
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
