dc639ed763e373e684d222882e9634c399e39feb
Carlos Lamego <clamego1@yahoo.com.br>
2022-04-09 17:35:33 -0300

    :sparkles: Cria feature Warlock.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { Warrior } from "./Warrior";
 import { Mage } from "./Mage";
 import { Hunter } from "./Hunter";
 import { Paladino } from "./Paladino";
+import { Warlock } from "./Warlock";
 import { Personagem } from "./Personagem";
 
 let mage: Personagem = new Mage("Angelo mage");
@@ -10,6 +11,7 @@ let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
 let hunter: Personagem = new Hunter("Clamego Hunter")
 let paladino: Personagem = new Paladino("Clamego Paladino")
+let warlock: Personagem = new Warlock("Clamego Bruxão")
 
 
 console.log("mage :>> ", mage);
@@ -17,3 +19,4 @@ console.log("warrior :>> ", warrior);
 console.log("priest :>> ", priest);
 console.log("hunter :>> ", hunter);
 console.log("paladino :>> ", paladino);
+console.log("warlock :>> ", warlock);
