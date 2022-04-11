b6f7b6f1e4b7f0a930efd96e7e58aac0189f8287
Matbm7 <Matheusbm00@icloud.com>
2022-04-10 23:21:19 -0300

    :memo: Atualiza personagens com novos atributos

--- Paladino.ts
+++ Paladino.ts
@@ -11,5 +11,7 @@ export class Paladino extends Personagem {
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
         this._intelecto = Util.randomizar(100, 1_000);
         this._velocidade = Util.randomizar(10, 2_000);
+        this._energy = Util.randomizar(100, 500);
+        this._cicatrizes = Util.randomizar(1, 2);
     }
 }
