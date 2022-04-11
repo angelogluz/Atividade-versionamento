4b319782bb0516326a000804393b442ee85a9caf
rodrigo.wiebbelling <rodrigo.wiebbelling@thema.inf.br>
2022-04-11 14:48:59 -0300

    :art: Add attribute mana to mage

--- Mage.ts
+++ Mage.ts
@@ -5,6 +5,7 @@ export class Mage extends Personagem {
     private _intelecto: number;
     private _velocidade: number;
     private _vitality: number;
+    private _mana: number;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
@@ -13,5 +14,6 @@ export class Mage extends Personagem {
         this._intelecto = Util.randomizar(100, 1_000);
         this._velocidade = Util.randomizar(10, 2_000);
         this._vitality = Util.randomizar(10, 200)
+        this._mana = Util.randomizar(10, 20)
     }
 }
