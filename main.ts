a65e03e89c6b1d10dba90e2394d0954c572bb374
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-04-12 09:46:34 -0300

    feat/Hunter class

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-import { Priest } from "./Priest";
-import { Warrior } from "./Warrior";
-import { Mage } from "./Mage";
-import { Personagem } from "./Personagem";
+import { Priest } from "./Priest"
+import { Warrior } from "./Warrior"
+import { Mage } from "./Mage"
+import { Hunter } from "./Hunter"
+import { Personagem } from "./Personagem"
 
-let mage: Personagem = new Mage("Angelo mage");
-let warrior: Personagem = new Warrior("Angelo warrior");
-let priest: Personagem = new Priest("Angelo priest");
+let mage: Personagem = new Mage("Angelo mage")
+let hunter: Personagem = new Hunter("Angelo hunter")
+let warrior: Personagem = new Warrior("Angelo warrior")
+let priest: Personagem = new Priest("Angelo priest")
 
-console.log("mage :>> ", mage);
-console.log("warrior :>> ", warrior);
-console.log("priest :>> ", priest);
+console.log("mage :>> ", mage)
+console.log("hunter :>> ", hunter)
+console.log("warrior :>> ", warrior)
+console.log("priest :>> ", priest)
