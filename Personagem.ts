18f83e37a10d485ad87cbb81c31a3b89822c6b36
Daniela <admdanniela@gmail.com>
2022-04-11 21:14:58 -0300

    :art: Initialize base fields
    Incluir aprimoramento no arquivo Personagem.ts

--- Personagem.ts
+++ Personagem.ts
@@ -4,11 +4,15 @@ export class Personagem {
     protected _armadura: number;
     protected _vidaMaxima: number;
     protected _vidaAtual: number;
+    protected _agilidade: number;
+    protected _forca: number;
 
     constructor(nome: string) {
         this._nome = nome;
         this._armadura = Util.randomizar(100, 1_000);
         this._vidaMaxima = Util.randomizar(100, 1_000);
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._agilidade = Util.randomizar(100, 1_000);
+        this._forca = Util.randomizar(100, 1_000);
     }
 }
