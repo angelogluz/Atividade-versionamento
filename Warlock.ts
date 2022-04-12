1b5a9a2ef959dca5acbc589d7d586e48a7f8a8fb
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-04-12 10:04:24 -0300

    ⚡️ melhora a classe Hunter e adiciona warlock

--- Warlock.ts
+++ Warlock.ts
@@ -1,18 +1,14 @@
 import { Personagem } from "./Personagem"
 import { Util } from "./Util"
 
-export class Hunter extends Personagem {
+export class Warlock extends Personagem {
     private _intelecto: number
     private _velocidade: number
-    private _forca: number
-    private _habilidade: number
-
     constructor(nome: string) {
         super(nome)
-        this._forca = Util.randomizar(100, 1_000)
+        this._armadura = Util.randomizar(100, 1_000)
         this._vidaMaxima = Util.randomizar(200, 1_000)
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima)
-        this._habilidade = Util.randomizar(100, 1_000)
         this._intelecto = Util.randomizar(100, 1_000)
         this._velocidade = Util.randomizar(10, 2_000)
     }
--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 import { Priest } from "./Priest"
 import { Warrior } from "./Warrior"
 import { Mage } from "./Mage"
-import { Hunter } from "./Hunter"
 import { Personagem } from "./Personagem"
+import { Hunter } from "./Hunter"
+import { Warlock } from "./Warlock"
 
 let mage: Personagem = new Mage("Angelo mage")
-let hunter: Personagem = new Hunter("Angelo hunter")
 let warrior: Personagem = new Warrior("Angelo warrior")
 let priest: Personagem = new Priest("Angelo priest")
+let hunter: Personagem = new Hunter("Angelo hunter")
+let warlock: Personagem = new Warlock("Angelo warlock")
 
 console.log("mage :>> ", mage)
-console.log("hunter :>> ", hunter)
 console.log("warrior :>> ", warrior)
 console.log("priest :>> ", priest)
+console.log("hunter :>> ", hunter)
+console.log("warlock :>> ", warlock)
