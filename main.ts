f12825ac23f11a501db12d3b5eea46f84764ca4c
JonasCouto <jonas-couto@hotmail.com>
2022-04-12 23:35:39 -0300

    :sparkles: InstaciaMage
    Add new mage

--- main.ts
+++ main.ts
@@ -4,9 +4,11 @@ import { Mage } from "./Mage";
 import { Personagem } from "./Personagem";
 
 let mage: Personagem = new Mage("Angelo mage");
+let mageJonas: Personagem = new Mage("Jonas mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
 
+console.log("mage :>> ", mageJonas);
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
 console.log("priest :>> ", priest);
