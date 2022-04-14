e207f668a5bad3dab96525ef4018c67b9b84a57b
Elias Wojahn <elias.wojahn@bridge-mt.com>
2022-04-14 19:08:46 -0300

    :memo: Update main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,14 @@ import { Priest } from "./Priest";
 import { Warrior } from "./Warrior";
 import { Mage } from "./Mage";
 import { Personagem } from "./Personagem";
+import { Hunter } from "./Hunter";
 
 let mage: Personagem = new Mage("Angelo mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
+let hunter: Personagem = new Hunter("Elias huntert");
 
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
 console.log("priest :>> ", priest);
+console.log("hunter :>> ", hunter);
