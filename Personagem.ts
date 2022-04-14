a62f5506813b4523626ab97e7656e7bb242604bf
Sandro Sória Canez <90696452+sandro-soria-canez@users.noreply.github.com>
2022-04-14 20:24:21 -0300

    adicionado vida infinita personagem

--- Personagem.ts
+++ Personagem.ts
@@ -4,11 +4,13 @@ export class Personagem {
     protected _armadura: number;
     protected _vidaMaxima: number;
     protected _vidaAtual: number;
+    protected _vidaInfinita: number;
 
     constructor(nome: string) {
         this._nome = nome;
         this._armadura = Util.randomizar(100, 1_000);
         this._vidaMaxima = Util.randomizar(100, 1_000);
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._vidaInfinita = Util.randomizar(40, this._vidaInfinita);
     }
 }
