3be9721580328bd303bd84fb244ab8e7d872893c
l4porta <brayanlaporta@gmail.com>
2022-04-14 16:52:25 -0300

    :sparkles: Adiciona atributo de escudo na classe Mago

--- Mage.ts
+++ Mage.ts
@@ -5,6 +5,7 @@ export class Mage extends Personagem {
     private _intelecto: number;
     private _velocidade: number;
     private _poder: number;
+    private _escudo: number;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(100, 1_000);
@@ -13,5 +14,6 @@ export class Mage extends Personagem {
         this._intelecto = Util.randomizar(100, 1_000);
         this._velocidade = Util.randomizar(10, 2_000);
         this._poder = Util.randomizar(20, 2_000);
+        this._escudo = Util.randomizar(20, 2_000);
     }
 }
