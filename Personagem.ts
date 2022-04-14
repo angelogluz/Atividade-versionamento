39682ac08215fe9a6c9f7d5a2ab3b9d75253e5db
Rafael Cândido <rafaelcandido51@gmail.com>
2022-04-14 20:47:45 -0300

    📝 Upadate Main.ts

--- Personagem.ts
+++ Personagem.ts
@@ -1,14 +1,31 @@
 import { Util } from "./Util";
+
 export class Personagem {
+
     protected _nome: string;
+
     protected _armadura: number;
+
     protected _vidaMaxima: number;
+
     protected _vidaAtual: number;
 
+    protected _poder: number:
+
+
+
     constructor(nome: string) {
+
         this._nome = nome;
+
         this._armadura = Util.randomizar(100, 1_000);
+
         this._vidaMaxima = Util.randomizar(100, 1_000);
+
         this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+
+        this._poder = Util.randomizar(100, 1_000);
+
     }
+
 }
