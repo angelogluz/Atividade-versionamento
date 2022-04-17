830927952c279c7d055719f092fc05f9452427f9
gabzsk2 <103905914+gabzsk2@users.noreply.github.com>
2022-04-17 19:47:11 -0300

    :bulb: Update Class Hunter
    Descrição
    -Melhoria na classe Hunter.
    
    Implementa
    -Adiciona atributo domar animais
    - Modifica o valor mínimo de precisão

--- Hunter.ts
+++ Hunter.ts
@@ -4,12 +4,14 @@ import { Util } from "./Util";
 export class Hunter extends Personagem {
     private _precisao: number;
     private _rastreamento: number;
+    private _domarAnimais: boolean;
     constructor(nome: string) {
         super(nome);
         this._armadura = Util.randomizar(150, 1_000);
         this._vidaMaxima = Util.randomizar(300, 1_000);
         this._vidaAtual = Util.randomizar(70, this._vidaMaxima);
-        this._precisao = Util.randomizar(100, 1_000);
+        this._precisao = Util.randomizar(500, 1_000);
         this._rastreamento = Util.randomizar(50, 2_000);
+        this._domarAnimais = true;
     }
 }
