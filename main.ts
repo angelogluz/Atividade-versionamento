fcadf881170006734ffeb0d6887d0a52467cf3e3
Matheus Massing <matheus_massing@hotmail.com>
2022-05-12 20:25:38 -0300

    :art: Refactor main class

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
-import { Priest } from "./Priest";
-import { Warrior } from "./Warrior";
+import { Hunter } from "./Hunter";
 import { Mage } from "./Mage";
 import { Personagem } from "./Personagem";
+import { Priest } from "./Priest";
+import { Warrior } from "./Warrior";
 
 let mage: Personagem = new Mage("Angelo mage");
 let warrior: Personagem = new Warrior("Angelo warrior");
 let priest: Personagem = new Priest("Angelo priest");
+let hunter: Personagem = new Hunter("Angelo hunter");
+
 
 console.log("mage :>> ", mage);
 console.log("warrior :>> ", warrior);
 console.log("priest :>> ", priest);
+console.log("hunter :>> ", hunter);
